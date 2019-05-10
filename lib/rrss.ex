c215640deb40daa25eb5561db024a739f677899d
julienrouzieres <julien.rouzieres@mac.com>
2019-05-10 16:03:15 +0200

    initial commit

---
+++ lib/rrss.ex
@@ -0,0 +1,18 @@
+defmodule RRSS do
+  @moduledoc """
+  Documentation for RRSS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RRSS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rrss_test.exs
@@ -0,0 +1,8 @@
+defmodule RRSSTest do
+  use ExUnit.Case
+  doctest RRSS
+
+  test "greets the world" do
+    assert RRSS.hello() == :world
+  end
+end
